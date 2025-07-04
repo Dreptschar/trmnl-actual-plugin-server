f8188b5ff5e20acfe21b7fcb09c392382c8aca2c
dreptschar <dreptschar@gmail.com>
2025-07-04 19:45:32 +0200

    debugging

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post('/api/markup',async (req, res)  => {
-    const body = req.body;
+    console.log(req)
+    const body = req.data;
     console.log(body)
     const data = await fetchData(body.serverURL, body.serverPassword, body.budgetSyncId, body.budgetEncryptionPassword);
     res.status(200).json(data);
