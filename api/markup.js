3a87fd032465224e52cd736d0c8a404f549a1a57
dreptschar <dreptschar@gmail.com>
2025-07-04 18:00:53 +0200

    added body log

--- api/markup.js
+++ api/markup.js
@@ -3,6 +3,7 @@ import { fetchData } from "../actual/actual-service";
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const body = req.body;
+    console.log(body)
     const data = await fetchData(body.serverURL, body.serverPassword, body.budgetSyncId, body.budgetEncryptionPassword);
     res.status(200).json(JSON.stringify(data));
   } else {
